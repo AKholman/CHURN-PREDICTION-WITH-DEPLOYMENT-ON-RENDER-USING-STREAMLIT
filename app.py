b4c192f1d31a4c6cb217182de5567ba87afa1883
Alex_DS <alexkh2017@gmail.com>
2025-09-28 02:15:35 -0500

    updated app.py

--- app.py
+++ app.py
@@ -25,31 +25,16 @@ with st.form("predict_form"):
     monthly_charges = st.number_input("monthly_charges", min_value=0.0, value=50.0)
     total_charges = st.number_input("total_charges", min_value=0.0, value=600.0)
     tenure_days = st.number_input("tenure_days", min_value=0, max_value=5000, value=365)
-
-    # categorical/binary features
-    contract_type = st.selectbox("Contract type", ["Month-to-month", "One year", "Two year"])
-    paperless_billing = st.radio("Paperless billing", ["Yes", "No"])
-    payment_method = st.selectbox("Payment method", 
-                                  ["Electronic check", "Mailed check", "Bank transfer", "Credit card"])
-    senior_citizen = st.radio("Senior citizen", ["Yes", "No"])
-    partner = st.radio("Has partner", ["Yes", "No"])
-    internet_service = st.selectbox("Internet service", ["DSL", "Fiber optic", "None"])
-    tech_support = st.selectbox("Tech support", ["Yes", "No", "None"])
     
     submitted = st.form_submit_button("Predict")
 
 if submitted:
     input_dict = {
-        "type": contract_type,
-        "paperless_billing": paperless_billing,
         "payment_method": payment_method,
         "monthly_charges": monthly_charges,
         "total_charges": total_charges,
         "tenure_days": tenure_days,
-        "senior_citizen": senior_citizen,
-        "partner": partner,
-        "internet_service": internet_service,
-        "tech_support": tech_support,
+  
         # fill the rest with default values or expose them in UI
     }
     df = pd.DataFrame([input_dict])
